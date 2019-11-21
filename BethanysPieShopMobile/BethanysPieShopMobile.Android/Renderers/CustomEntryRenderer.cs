f60fb4a8749a4250842459f19d42d5ed49e7daac
ECR\Egon Rasmussen <ecr@live.dk>
2019-11-21 09:40:42 +0100

    ok

---
+++ BethanysPieShopMobile/BethanysPieShopMobile.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using BethanysPieShopMobile;
+using BethanysPieShopMobile.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace BethanysPieShopMobile.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control?.SetBackgroundColor(global::Android.Graphics.Color.Aqua);
+        }
+    }
+}
--- BethanysPieShopMobile/BethanysPieShopMobile/App.xaml.cs
+++ BethanysPieShopMobile/BethanysPieShopMobile/App.xaml.cs
@@ -11,7 +11,7 @@ namespace BethanysPieShopMobile
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
