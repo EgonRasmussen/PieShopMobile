b51c2595a17d58c01403bfafc7bef343c467058a
ECR\EgonRasmussen <ecr@eucsyd.dk>
2021-01-26 17:53:45 +0100

    v5.0

--- BethanysPieShopMobile/BethanysPieShopMobile/PieDetailPage.xaml.cs
+++ BethanysPieShopMobile/BethanysPieShopMobile/PieDetailPage.xaml.cs
@@ -20,8 +20,7 @@ namespace BethanysPieShopMobile
             Pie = new Pie
             {
                 Id = 1,
-                Description =
-                    "Gingerbread jujubes donut. Gummies cake halvah. Jujubes candy canes pudding cupcake ice cream bonbon chocolate bar. Pudding apple pie carrot cake lollipop. Caramels sugar plum muffin croissant cake dragée carrot cake jelly-o cotton candy. Jelly beans chocolate pie bear claw donut sesame snaps. Carrot cake tart sweet gummies. Lollipop cotton candy muffin marshmallow chocolate bar danish. Tart donut fruitcake. Toffee candy danish chocolate danish toffee chocolate bar. Ice cream chocolate cake gingerbread. Brownie tootsie roll cotton candy cookie bear claw cheesecake cupcake lollipop apple pie.",
+                Description = "Gingerbread jujubes donut. Gummies cake halvah. Jujubes candy canes pudding cupcake ice cream bonbon chocolate bar. Pudding apple pie carrot cake lollipop. Caramels sugar plum muffin croissant cake dragée carrot cake jelly-o cotton candy. Jelly beans chocolate pie bear claw donut sesame snaps. Carrot cake tart sweet gummies. Lollipop cotton candy muffin marshmallow chocolate bar danish. Tart donut fruitcake. Toffee candy danish chocolate danish toffee chocolate bar. Ice cream chocolate cake gingerbread. Brownie tootsie roll cotton candy cookie bear claw cheesecake cupcake lollipop apple pie.",
                 ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/applepiesmall.jpg",
                 InStock = true,
                 AvailableFromDate = new DateTime(2018, 12, 1),
@@ -29,7 +28,7 @@ namespace BethanysPieShopMobile
                 Price = 20.95
             };
             this.BindingContext = Pie;
-            //this.BindingContext = this;   // #2 BINDING VIA PAGE TO SINGLE OBJECT
+            //this.BindingContext = this;   // #2 BINDING VIA PAGE AND PROPERTY TO SINGLE OBJECT
             #endregion
 
             #region #3 BINDING TO VIEWMODEL
