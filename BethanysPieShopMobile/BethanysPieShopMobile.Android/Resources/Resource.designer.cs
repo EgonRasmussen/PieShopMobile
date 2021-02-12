80630b31906b74e321f1484973de78b992acd790
ECR\EgonRasmussen <ecr@eucsyd.dk>
2021-02-12 09:27:31 +0100

    ok

--- BethanysPieShopMobile/BethanysPieShopMobile.Android/Resources/Resource.designer.cs
+++ BethanysPieShopMobile/BethanysPieShopMobile.Android/Resources/Resource.designer.cs
@@ -11228,13 +11228,16 @@ namespace BethanysPieShopMobile.Droid
 			public const int notify_panel_notification_icon_bg = 2131165324;
 			
 			// aapt resource value: 0x7F07008D
-			public const int test_custom_background = 2131165325;
+			public const int patternwide = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int tooltip_frame_dark = 2131165326;
+			public const int test_custom_background = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int tooltip_frame_light = 2131165327;
+			public const int tooltip_frame_dark = 2131165327;
+			
+			// aapt resource value: 0x7F070090
+			public const int tooltip_frame_light = 2131165328;
 			
 			static Drawable()
 			{
